01f8c52ce337e27465fd08b6bfe806eb54bb489f
Anton Kenderov <133912013+tonkataaa@users.noreply.github.com>
2023-05-20 23:29:06 +0300

    Add files via upload

---
+++ 1/First Steps in Coding - Exercise/02. Radians to Degrees/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.Radians_to_Degrees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Четем радиани от конзолата
+            double radians = double.Parse(Console.ReadLine());
+            //2. Преобразуваме от радиани в градуси (градус = радиан * 180 / π.)
+            double degrees = radians * 180 / Math.PI;
+            //3. Отпечатваме градусите на конзолата
+            Console.WriteLine(degrees);
+        }
+    }
+}
